890a0fbb685afc736c6331a2962285a590ce0484
Jose Luis <wildchamo@gmail.com>
2021-12-31 13:00:19 -0500

    preparación proyecto

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
-function App(props) {
+function App() {
   return (
-    <div className="App">
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          {props.children}
-        </a>
-      </header>
-
-    </div>
   );
 }
 
