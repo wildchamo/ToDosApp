3211990eae74c063da02bb26be759f33210546fe
Jose Luis <wildchamo@gmail.com>
2021-12-31 13:10:27 -0500

    base del componente app y utilización de React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 //import './App.css';
+import React from 'react';
+
+
+const todos=[
+  {text: 'Jugar cartas',completed:false},
+  {text: 'Estudiar Platzi',completed:false},
+  {text: 'Hacer algo de comer',completed:false},
+]
 
 function App() {
   return (
+    <React.Fragment>
+    
+    <TodoCounter/>
+    <h2>Has completado 2 de 3 TODOs</h2>
+    <TodoSearch/>
+    <input placeholder="Hola nombreUsuario"></input>
+    <TodoList>
+      {todos.map(todo => (
+        <TodoItem/>
+      ))}
+    <TodoList/>
+    <CreateTodoButton/>
+    <button>+</button>
 
-  );
+    </React.Fragment>
+      );
 }
 
 export default App;
