b980773c538eaa4b5bc67dba1413aed0bd6ef1ee
Jose Luis <wildchamo@gmail.com>
2022-01-01 11:43:38 -0500

    estilos con hoa de estilos aparte

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'TodoCounter.css';
+import './TodoCounter.css';
 
 const styles={
     color: 'red',
@@ -8,7 +8,7 @@ const styles={
 
 function TodoCounter(){
     return(
-        <h2 style={styles}>Has completado 2 de 3 TODOs</h2>
+        <h2 className="TodoCounter">Has completado 2 de 3 TODOs</h2>
         );
 }
 export  {TodoCounter};
