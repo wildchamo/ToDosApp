096a1e8e4e410823788f07eb79b844f8c258e6d4
Jose Luis <wildchamo@gmail.com>
2022-01-02 11:28:33 -0500

    eventos utilizando variables en react

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './CreateTodoButton.css';
 
-
+//si directamente escribís el codigo sin envolverlo en una función se ejecuta automaticamente!
 function CreateTodoButton(){
+
+    const onClickButton = () =>{
+        alert('Aquí se debería abrir el modal!');
+    }
+
     return(
         <button
         className="CreateTodoButton"
-        onClick={()=> console.log('Hubo un click en el boton!')}
+        onClick={onClickButton}
         >+</button>
         );
 }
