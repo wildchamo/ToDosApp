7c190a3834aa42adaa63f60fc53f7f25d0410168
Jose Luis <wildchamo@gmail.com>
2022-01-02 11:25:37 -0500

    creación de eventos en linea con react

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -4,7 +4,10 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton(){
     return(
-        <button className="CreateTodoButton">+</button>
+        <button
+        className="CreateTodoButton"
+        onClick={()=> console.log('Hubo un click en el boton!')}
+        >+</button>
         );
 }
 export  {CreateTodoButton};
